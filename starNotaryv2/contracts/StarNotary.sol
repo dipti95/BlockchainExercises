24cfc07ccc4470a0f5fa8d7215193bc3c046ba77
dipti <dipti.yadav@outlook.com>
2022-05-09 14:12:49 -0400

    ERC721 contract

--- starNotaryv2/contracts/StarNotary.sol
+++ starNotaryv2/contracts/StarNotary.sol
@@ -1,17 +1,30 @@
+//Truffle v5.5.12 - a development framework for Ethereum
+
 pragma solidity >=0.4.24;
 
+//Importing openzeppelin-solidity ERC-721 implemented Standard
+//import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 
-contract StarNotary is ERC721 {
+// StarNotary Contract declaration inheritance the ERC721 openzeppelin implementation
+abstract contract StarNotary is ERC721 {
 
+    // Star data
     struct Star {
         string name;
     }
 
+    // Implement Task 1 Add a name and symbol properties
+    // name: Is a short name to your token
+    // symbol: Is a short string like 'USD' -> 'American Dollar'
+    
+
+    // mapping the Star with the Owner Address
     mapping(uint256 => Star) public tokenIdToStarInfo;
+    // mapping the TokenId and price
     mapping(uint256 => uint256) public starsForSale;
 
-
+    
     // Create Star using the Struct
     function createStar(string memory _name, uint256 _tokenId) public { // Passing the name and tokenId as a parameters
         Star memory newStar = Star(_name); // Star is an struct so we are creating a new Star
@@ -28,7 +41,7 @@ contract StarNotary is ERC721 {
 
     // Function that allows you to convert an address into a payable address
     function _make_payable(address x) internal pure returns (address payable) {
-        return address(uint160(x));
+           return payable(address(uint160(x)));
     }
 
     function buyStar(uint256 _tokenId) public  payable {
@@ -36,12 +49,30 @@ contract StarNotary is ERC721 {
         uint256 starCost = starsForSale[_tokenId];
         address ownerAddress = ownerOf(_tokenId);
         require(msg.value > starCost, "You need to have enough Ether");
-        _transferFrom(ownerAddress, msg.sender, _tokenId); // We can't use _addTokenTo or_removeTokenFrom functions, now we have to use _transferFrom
+        transferFrom(ownerAddress, msg.sender, _tokenId); // We can't use _addTokenTo or_removeTokenFrom functions, now we have to use _transferFrom
         address payable ownerAddressPayable = _make_payable(ownerAddress); // We need to make this conversion to be able to use transfer() function to transfer ethers
         ownerAddressPayable.transfer(starCost);
         if(msg.value > starCost) {
-            msg.sender.transfer(msg.value - starCost);
+           payable(msg.sender).transfer(msg.value - starCost);
         }
     }
 
+    // Implement Task 1 lookUptokenIdToStarInfo
+    function lookUptokenIdToStarInfo (uint _tokenId) public view returns (string memory) {
+        //1. You should return the Star saved in tokenIdToStarInfo mapping
+    }
+
+    // Implement Task 1 Exchange Stars function
+    function exchangeStars(uint256 _tokenId1, uint256 _tokenId2) public {
+        //1. Passing to star tokenId you will need to check if the owner of _tokenId1 or _tokenId2 is the sender
+        //2. You don't have to check for the price of the token (star)
+        //3. Get the owner of the two tokens (ownerOf(_tokenId1), ownerOf(_tokenId2)
+        //4. Use _transferFrom function to exchange the tokens.
+    }
+
+    // Implement Task 1 Transfer Stars
+    function transferStar(address _to1, uint256 _tokenId) public {
+        //1. Check if the sender is the ownerOf(_tokenId)
+        //2. Use the transferFrom(from, to, tokenId); function to transfer the Star
+    }
 }
